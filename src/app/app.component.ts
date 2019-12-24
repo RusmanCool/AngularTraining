8f3fd3e6197bda06a53477db83e005d39b6b324a
Rusman Cool <rusmancool@yahoo.com>
2019-12-24 16:06:49 -0500

    Added a template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import {Component} from '@angular/core';
 
 @Component({
-    selector: 'app-root',
-    template: '<h1>My App from LinkedIn Learning Course</h1>'
+    selector: 'mw-app',
+    templateUrl: './app.component.html'
 })
 export class AppComponent {}
